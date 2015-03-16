17f3729d0b7570a7f79106a895e92f92e9f3e5d3
ßÀÌî¤µ¤ó <k-hamano@biglobe.co.jp>
2015-03-16 20:59:26 +0900

    StringUtilsのテストコードを追加

--- JunitExercises/test/junit/exercises/CalculatorTest.java
+++ JunitExercises/test/junit/exercises/CalculatorTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 
 public class CalculatorTest {
     
+    // 例外のテストコードはアノテーションで指定する
     @Test(expected = IllegalArgumentException.class)
     public void divideの第二引数に0を指定すると例外が発生する() throws Exception {
         Calculator sut = new Calculator();
