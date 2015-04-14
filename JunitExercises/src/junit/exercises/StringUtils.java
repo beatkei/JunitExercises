78cc52263037b765333c51e02f651e815ef83f38
ßÀÌî¤µ¤ó <k-hamano@biglobe.co.jp>
2015-04-14 21:01:23 +0900

    CounterとCounterのテストコードを追加

--- JunitExercises/src/junit/exercises/StringUtils.java
+++ JunitExercises/src/junit/exercises/StringUtils.java
@@ -19,5 +19,25 @@ public class StringUtils {
         // こっちはStringクラスのreplaceFirstで１個目に正規表現を指定
         return snake.replaceFirst("^_", "");
     }
+
+    public static boolean compareStringBuilder(String text) {
+        String stringText = text;
+        StringBuilder stringBuilderText1 = new StringBuilder(text);
+        StringBuilder stringBuilderText2 = new StringBuilder(text);
+        // StringとStringBuilderの比較はStringクラスのcontentEqualsを使う
+        Boolean result1 = stringText.contentEquals(stringBuilderText1);
+        // StringBuilder同士の比較はStringオブジェクトに変換してからontentEqualsを使う
+        Boolean result2 = stringBuilderText1.toString().contentEquals(stringBuilderText2);
+        if (result1 != true | result2 != true) return false;
+        return true;
+    }
+    
+    public static String comvertIntegerToString(int i) {
+        // その１　String.valueOf()を使ってStringに変換する
+        String convertText1 = String.valueOf(i);
+        // その２　Integer.toString()を使ってStringに変換する
+        String convertText2 = Integer.toString(i);
+        return convertText1;
+    }
     
 }
